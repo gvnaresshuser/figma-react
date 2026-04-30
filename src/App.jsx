c75ae0f4e752ead303ecdee7f28a9c6d47d5f9bc
gvnaresshuser <gvnaressh@gmail.com>
2026-04-30 14:38:55 +0530

    Initial commit - React project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import AwesomeComponent from "./AwesomeComponent";
+
+function App() {
+
+  return (
+    <>
+      <AwesomeComponent />
+    </>
+  );
+}
+
+export default App;
